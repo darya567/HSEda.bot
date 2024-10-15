ebba7da58bb055afb1e4cb49495d597282ab6797
Polina-Gor <polina.goryacheva2006@gmail.com>
2024-10-15 19:09:40 +0300

    Update restaurants.py

--- restaurants.py
+++ restaurants.py
@@ -5,10 +5,10 @@ from restaurant_model import Restaurant
 restaurants = [
         Restaurant(
             id=1, 
-            title='ты',
-            description='тык',
-            photo_url='https://cs10.pikabu.ru/post_img/2019/02/08/4/1549601555162274027.jpg',
-            address='тык тык тык'
+            title='Вспышка',
+            description='Первая пышечная в Нижнем Новгороде, в которой можно купить невероятно вкусные и свежие пышки всего по 30 рублей за 1 штуку',
+            photo_url='https://avatars.mds.yandex.net/get-altay/10447847/2a0000018afa0f65d8cbcc3f30398879460b/L_height',
+            address='Алексеевская, 11'
             ),
         
 
