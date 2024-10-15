2ac4bf4b75f18fb4051dd6c909869c702b83f739
Polina-Gor <polina.goryacheva2006@gmail.com>
2024-10-15 19:14:59 +0300

    Update restaurants.py

--- restaurants.py
+++ restaurants.py
@@ -8,7 +8,14 @@ restaurants = [
             title='Вспышка',
             description='Первая пышечная в Нижнем Новгороде, в которой можно купить невероятно вкусные и свежие пышки всего по 30 рублей за 1 штуку',
             photo_url='https://avatars.mds.yandex.net/get-altay/10447847/2a0000018afa0f65d8cbcc3f30398879460b/L_height',
-            address='Алексеевская, 11'
+            address='Нижний Новгород, Алексеевская, 11'
+            ),
+         Restaurant(
+            id=2, 
+            title='Ёмаё',
+            description='Группа компаний «Ё-маё» гордо шагает по Нижнему Новгороду с 2013 года. Именно тогда, 17 марта,  открылось наше первое заведение «Пельмени и вареники Ё-маё». С тех пор нашей главной задачей является не только накормить своих посетителей вкусной и разнообразной едой, но и украсить их досуг, сделав его более веселым и доступным',
+            photo_url='https://lh3.googleusercontent.com/gps-proxy/ALd4DhHrxNqqhSrNcJEVod2bLbN_wsKuzXnKa1tbE41XctLJqk8k8BLvtGUP39lMaYtNKEitNnwyy_vOAb5Z_AQvbgA-VsrK7IKKGSWY3uC54EMKgsWBHWpNGABQJFPZFa4tKI7xGtFTWvPo-WTKIc_zc-7hqlEXVKdTqUN-sYJ2D8Q1ZlyUH8yjYDnNXQ=s1360-w1360-h1020',
+            address='Нижний Новгород, Большая Покровская ул., 22'
             ),
         
 
