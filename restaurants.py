4774b5d2469cca129ebf4e8ceac35d4ba2616d0e
teplakovae <teplakovae281@gmail.com>
2024-10-15 22:17:13 +0300

    Добавила 3 ресторана

--- restaurants.py
+++ restaurants.py
@@ -18,5 +18,26 @@ restaurants = [
             address='Нижний Новгород, Большая Покровская ул., 22'
             ),
         
-
+         Restaurant(
+            id=3, 
+            title='Лепи Тесто',
+            description='Даже привычный продукт при внимании к деталям обретает дополнительную гастрономическую ценность. Лепи тесто-это новая модная пельменная,которая никого не оставит равнодушным.',
+            photo_url='https://a-a-ah-ru.s3.amazonaws.com/uploads/items/142565/291914/large_XXXL.jpg',
+            address='Нижний Новгород, ул. Большая Покровская, 47 \ Нижний Новгород, ул. Минина, 12 \ Нижний Новгород, ул. Героя Фильченкова, 12'
+            ),
+         Restaurant(
+            id=4, 
+            title='Skuratov Coffee Roasters',
+            description='Skuratov Coffee — это сеть стильных кофеен, которые полюбились фрилансерам и студентам. В каждой кофейне есть своя фишка в интерьере и неизменно хороший кофе',
+            photo_url='https://cdn-imgproxy.mamado.su/gOit_8WDiD-nZ2Oxs0xTdf0Ls3LtAN8Fxli_KMlicvs/rs:fit:2000:2000:1/g:ce/q:90/czM6Ly9tYW1hZG8t/YXBpLXByb2R1Y3Rp/b24vc3RvcmFnZS8x/MTE4MTc2L1hYWEwu/anBlZw.jpg',
+            address='Нижний Новгород, ул. Большая Покровская, 2 \ Нижний Новгород, ул. Большая Печерская, 44 \ Нижний Новгород, ул. Ковалихинская, 4А \ Нижний Новгород,ул. Верхне-Волжская набережная, 2а'
+            ),
+         Restaurant(
+            id=5, 
+            title='Noot',
+            description='Кафе Noot — это место, где вы можете насладиться вкусной и свежей едой, а также уютной атмосферой и дружелюбным персоналом',
+            photo_url='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/ea/b1/4a/caption.jpg',
+            address='Нижний Новгород, ул. Большая Покровская, 15'
+            ),
+         
 ]
