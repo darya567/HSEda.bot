018e530a0325aacedca1e0aec40dda71dd5ac0e2
teplakovae <teplakovae281@gmail.com>
2024-10-16 10:32:32 +0300

    Добавила 3 ресторана (подправила код)

--- restaurants.py
+++ restaurants.py
@@ -17,23 +17,29 @@ restaurants = [
             photo_url='https://lh3.googleusercontent.com/gps-proxy/ALd4DhHrxNqqhSrNcJEVod2bLbN_wsKuzXnKa1tbE41XctLJqk8k8BLvtGUP39lMaYtNKEitNnwyy_vOAb5Z_AQvbgA-VsrK7IKKGSWY3uC54EMKgsWBHWpNGABQJFPZFa4tKI7xGtFTWvPo-WTKIc_zc-7hqlEXVKdTqUN-sYJ2D8Q1ZlyUH8yjYDnNXQ=s1360-w1360-h1020',
             address='Нижний Новгород, Большая Покровская ул., 22'
             ),
-        
          Restaurant(
-            id=3, 
+            id=3,
+            title='Местечко моей мечты',
+            description='«Местечко моей мечты» — это заведение быстрого питания, где можно вкусно и сытно перекусить. Гостям нравится местная шаурма, особенно шаурма с жульеном, а также бистро с курицей или мясом.Средний чек выходит всего на 350 рублей!!!',
+            photo_url='https://yandex.ru/maps/org/mestechko_moyey_mechty/231615246267/gallery/?ll=44.032917%2C56.323049&photos%5Bbusiness%5D=231615246267&photos%5Bid%5D=urn%3Ayandex%3Asprav%3Aphoto%3ASpUu9M08MTrTaFRaduKv80SxmxthMQupl&z=15',
+            address='Нижний Новгород, Большая Печёрская ул., 48А'
+        ),
+         Restaurant(
+            id=4, 
             title='Лепи Тесто',
             description='Даже привычный продукт при внимании к деталям обретает дополнительную гастрономическую ценность. Лепи тесто-это новая модная пельменная,которая никого не оставит равнодушным.',
             photo_url='https://a-a-ah-ru.s3.amazonaws.com/uploads/items/142565/291914/large_XXXL.jpg',
             address='Нижний Новгород, ул. Большая Покровская, 47 \ Нижний Новгород, ул. Минина, 12 \ Нижний Новгород, ул. Героя Фильченкова, 12'
             ),
          Restaurant(
-            id=4, 
+            id=5, 
             title='Skuratov Coffee Roasters',
             description='Skuratov Coffee — это сеть стильных кофеен, которые полюбились фрилансерам и студентам. В каждой кофейне есть своя фишка в интерьере и неизменно хороший кофе',
             photo_url='https://cdn-imgproxy.mamado.su/gOit_8WDiD-nZ2Oxs0xTdf0Ls3LtAN8Fxli_KMlicvs/rs:fit:2000:2000:1/g:ce/q:90/czM6Ly9tYW1hZG8t/YXBpLXByb2R1Y3Rp/b24vc3RvcmFnZS8x/MTE4MTc2L1hYWEwu/anBlZw.jpg',
             address='Нижний Новгород, ул. Большая Покровская, 2 \ Нижний Новгород, ул. Большая Печерская, 44 \ Нижний Новгород, ул. Ковалихинская, 4А \ Нижний Новгород,ул. Верхне-Волжская набережная, 2а'
             ),
          Restaurant(
-            id=5, 
+            id=6, 
             title='Noot',
             description='Кафе Noot — это место, где вы можете насладиться вкусной и свежей едой, а также уютной атмосферой и дружелюбным персоналом',
             photo_url='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/ea/b1/4a/caption.jpg',
