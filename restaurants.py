d17c0b6de109c98676469d98c06d36b11040761a
teplakovae <teplakovae281@gmail.com>
2024-10-17 19:47:21 +0300

    Добавила 2 ресторана

--- restaurants.py
+++ restaurants.py
@@ -38,5 +38,19 @@ restaurants = [
             photo_url='https://aaanya.ru/wp-content/uploads/2018/05/Sovok.jpg',
             address='Нижний Новгород, Большая Покровская ул., 2'
             ),
+         Restaurant(
+            id=7, 
+            title='Лепи Тесто',
+            description='Даже привычный продукт при внимании к деталям обретает дополнительную гастрономическую ценность. Лепи тесто-это новая модная пельменная,которая никого не оставит равнодушным.',
+            photo_url='https://a-a-ah-ru.s3.amazonaws.com/uploads/items/142565/291914/large_XXXL.jpg',
+            address='Нижний Новгород, ул. Большая Покровская, 47 \ Нижний Новгород, ул. Минина, 12 \ Нижний Новгород, ул. Героя Фильченкова, 12'
+            ),
+         Restaurant(
+            id=8,  
+            title='Skuratov Coffee Roasters',
+            description='Skuratov Coffee — это сеть стильных кофеен, которые полюбились фрилансерам и студентам. В каждой кофейне есть своя фишка в интерьере и неизменно хороший кофе',
+            photo_url='https://cdn-imgproxy.mamado.su/gOit_8WDiD-nZ2Oxs0xTdf0Ls3LtAN8Fxli_KMlicvs/rs:fit:2000:2000:1/g:ce/q:90/czM6Ly9tYW1hZG8t/YXBpLXByb2R1Y3Rp/b24vc3RvcmFnZS8x/MTE4MTc2L1hYWEwu/anBlZw.jpg',
+            address='Нижний Новгород, ул. Большая Покровская, 2 \ Нижний Новгород, ул. Большая Печерская, 44 \ Нижний Новгород, ул. Ковалихинская, 4А \ Нижний Новгород,ул. Верхне-Волжская набережная, 2а'
+            ),
 
 ]
