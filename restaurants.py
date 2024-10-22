48c5d2a5e31bb66dc7259d24a9a407493591a608
teplakovae <teplakovae281@gmail.com>
2024-10-22 21:15:47 +0300

    Обновила средние чеки ресторанов

--- restaurants.py
+++ restaurants.py
@@ -49,7 +49,7 @@ restaurants = [
             description='Кафе Noot — это место, где вы можете насладиться вкусной и свежей едой, а также уютной атмосферой и дружелюбным персоналом.',
             photo_url='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/ea/b1/4a/caption.jpg',
             address='Нижний Новгород, ул. Большая Покровская, 15',
-            avg_price=200.0
+            avg_price=400.0
             ),
          Restaurant(
             id=7, 
@@ -57,7 +57,7 @@ restaurants = [
             description='Даже привычный продукт при внимании к деталям обретает дополнительную гастрономическую ценность. Лепи тесто-это новая модная пельменная,которая никого не оставит равнодушным.',
             photo_url='https://a-a-ah-ru.s3.amazonaws.com/uploads/items/142565/291914/large_XXXL.jpg',
             address='Нижний Новгород, ул. Большая Покровская, 47 \ Нижний Новгород, ул. Минина, 12 \ Нижний Новгород, ул. Героя Фильченкова, 12',
-            avg_price=200.0
+            avg_price=500.0
             ),
          Restaurant(
             id=8,  
@@ -65,7 +65,7 @@ restaurants = [
             description='Skuratov Coffee — это сеть стильных кофеен, которые полюбились фрилансерам и студентам. В каждой кофейне есть своя фишка в интерьере и неизменно хороший кофе',
             photo_url='https://cdn-imgproxy.mamado.su/gOit_8WDiD-nZ2Oxs0xTdf0Ls3LtAN8Fxli_KMlicvs/rs:fit:2000:2000:1/g:ce/q:90/czM6Ly9tYW1hZG8t/YXBpLXByb2R1Y3Rp/b24vc3RvcmFnZS8x/MTE4MTc2L1hYWEwu/anBlZw.jpg',
             address='Нижний Новгород, ул. Большая Покровская, 2 \ Нижний Новгород, ул. Большая Печерская, 44 \ Нижний Новгород, ул. Ковалихинская, 4А \ Нижний Новгород,ул. Верхне-Волжская набережная, 2а',
-            avg_price=200.0
+            avg_price=300.0
             ),
         Restaurant(
             id=9, 
@@ -73,7 +73,7 @@ restaurants = [
             description='Coffee Cake - место, где можно отвлечься от всего. С хорошим кофе, вкусной выпечкой и дружелюбным персоналом. Место, куда захочется возвращаться снова и снова.',
             photo_url='https://static.tildacdn.com/tild3135-3433-4830-a531-623366663331/A062D844-4980-4005-8.JPG',
             address='Нижний Новгород, ул. Большая Покровская, 2 \ Нижний Новгород, аэропорт Стригино \ Нижний Новгород, Парк "Швейцария"',
-            avg_price=200.0
+            avg_price=700.0
             ),
         Restaurant(
             id=10, 
@@ -81,7 +81,7 @@ restaurants = [
             description='Кафе Biblioteca находится на третьем этаже книжного магазина «Дирижабль» и предлагает своим гостям широкий выбор блюд, включая пиццу, пасту, лазанью, салаты и десерты, такие как чизкейки и крем-брюле.',
             photo_url='https://galina-lukas.ru/sites/default/files/568/img_5225.jpg',
             address='Нижний Новгород, ул. Большая Покровская, 46',
-            avg_price=200.0
+            avg_price=1000.0
             ),
         Restaurant(
             id=11, 
