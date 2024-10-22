61f24608c142892d88b5bfec99ccf454bb1c5498
Lebedeva2006 <ml7542190@gmail.com>
2024-10-22 23:18:25 +0300

    update

--- keyboard.py
+++ keyboard.py
@@ -5,9 +5,9 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 def get_categories():
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text='Рестораны с средним чеком до 500руб')],
-            [KeyboardButton(text='Рестораны с средним чеком от 500 до 1000руб')],
-            [KeyboardButton(text='Рестораны с средним чеком от 1000руб')]
+            [KeyboardButton(text='Рестораны со средним чеком до 500 р.')],
+            [KeyboardButton(text='Рестораны со средним чеком от 500 до 1000 р.')],
+            [KeyboardButton(text='Рестораны со средним чеком от 1000 р.')]
         ],
         resize_keyboard=True
     )
